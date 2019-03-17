8e13f6f4d94b37b05383761b214b33cd39adf5ea
MykoVol <Dfds43*fsb>
2019-03-17 13:15:35 +0200

    [b-3] Get all genres add method /v1/genre to return all movie genres available

---
+++ src/test/java/com/mykovolod/movieland/web/controller/GenreControllerTest.java
@@ -0,0 +1,55 @@
+package com.mykovolod.movieland.web.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("classpath:test-servlet-context.xml")
+public class GenreControllerTest {
+
+    @Value("${random.movies.limit}")
+    private int limit;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac)
+                .alwaysDo(print())
+                .build();
+    }
+
+    @Test
+    public void givenMovies_whenGetAll_thenMatch() throws Exception {
+
+        mockMvc.perform(get("/genre"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$", hasSize(6)))
+                .andExpect(jsonPath("$[*].id", containsInAnyOrder(1, 2, 3, 4, 5, 6)))
+                .andExpect(jsonPath("$[*].name", containsInAnyOrder("драма", "криминал",
+                        "детектив", "мелодрама", "биография", "комедия")))
+        ;
+    }
+
+}
--- src/test/resources/db/test-data.sql
+++ src/test/resources/db/test-data.sql
@@ -1,5 +1,7 @@
+-- ================== MOVIES ==========================
 INSERT INTO movieland.movies
-(name,
+(movie_id,
+ name,
  name_original,
  year,
  country,
@@ -7,7 +9,8 @@ INSERT INTO movieland.movies
  rating,
  price,
  picture_path)
-VALUES ('Побег из Шоушенка',
+VALUES (1,
+        'Побег из Шоушенка',
         'The Shawshank Redemption',
         '1994',
         'США',
@@ -15,9 +18,9 @@ VALUES ('Побег из Шоушенка',
         8.90,
         123.45,
         'https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1._SY209_CR0,0,140,209_.jpg');
-
 INSERT INTO movieland.movies
-(name,
+(movie_id,
+ name,
  name_original,
  year,
  country,
@@ -25,7 +28,8 @@ INSERT INTO movieland.movies
  rating,
  price,
  picture_path)
-VALUES ('Зеленая миля',
+VALUES (2,
+        'Зеленая миля',
         'The Green Mile',
         '1999',
         'США',
@@ -33,9 +37,9 @@ VALUES ('Зеленая миля',
         8.88,
         134.67,
         'https://images-na.ssl-images-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BanBnXkFtZTYwOTU2NTY3._V1._SY209_CR0,0,140,209_.jpg');
-
 INSERT INTO movieland.movies
-(name,
+(movie_id,
+ name,
  name_original,
  year,
  country,
@@ -43,7 +47,8 @@ INSERT INTO movieland.movies
  rating,
  price,
  picture_path)
-VALUES ('Форрест Гамп',
+VALUES (3,
+        'Форрест Гамп',
         'Forrest Gump',
         '1994',
         'США',
@@ -51,3 +56,67 @@ VALUES ('Форрест Гамп',
         8.6,
         200.60,
         'https://images-na.ssl-images-amazon.com/images/M/MV5BMTUxMzQyNjA5MF5BMl5BaFgfdgfdnBnXkFtZTYwOTU2NTY3._V1._09_.jpg');
+
+-- ================== GENRE ==========================
+INSERT INTO movieland.genres
+(genre_id,
+ name)
+VALUES (1,
+        'драма');
+INSERT INTO movieland.genres
+(genre_id,
+ name)
+VALUES (2,
+        'криминал');
+INSERT INTO movieland.genres
+(genre_id,
+ name)
+VALUES (3,
+        'детектив');
+INSERT INTO movieland.genres
+(genre_id,
+ name)
+VALUES (4,
+        'мелодрама');
+INSERT INTO movieland.genres
+(genre_id,
+ name)
+VALUES (5,
+        'биография');
+INSERT INTO movieland.genres
+(genre_id,
+ name)
+VALUES (6,
+        'комедия');
+
+-- ================== MOVIE_GENRE ==========================
+INSERT INTO movieland.movie_genre
+(movie_id,
+ genre_id)
+VALUES (1,
+        1);
+INSERT INTO movieland.movie_genre
+(movie_id,
+ genre_id)
+VALUES (1,
+        4);
+INSERT INTO movieland.movie_genre
+(movie_id,
+ genre_id)
+VALUES (1,
+        2);
+INSERT INTO movieland.movie_genre
+(movie_id,
+ genre_id)
+VALUES (2,
+        5);
+INSERT INTO movieland.movie_genre
+(movie_id,
+ genre_id)
+VALUES (3,
+        5);
+INSERT INTO movieland.movie_genre
+(movie_id,
+ genre_id)
+VALUES (3,
+        4);
